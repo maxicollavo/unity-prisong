c08b305ae583486723503cfaf122a4278f9fc9cd
bauutyyy <bautyakerman@gmail.com>
2023-06-04 00:39:25 -0300

    niebla oscura aplicada(ya deletee el cielo)
    ya hice lo de la niebla y el codigo, cree un script nuevo que c llama nieblatest, y un objeto basio que es NIEBLA, el tema es que no puedo probar nada porque sigo teniendo esos problemas con el codigo que no me los toma, pero ta , ya esta hecho, lo unico que hay que haces es desde el inspector arrastrarle el script a NIEBLA y de ahi arrastrarle el player y despues subirle el numero y que solo el afecte a el mundo de abajo

---
+++ Prisong/Assets/NieblaTest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NieblaTest : MonoBehaviour
+{
+    public Tranform Player
+    public float minDist;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+        float dist = Vector3.Distance(Player.position, transform.position);
+        
+        if (dist < minDist) 
+        {
+            RenderSettings.fog = true;
+        
+        
+        }
+        else
+        RenderSettings.fog = false;
+        
+    
+    
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, minDist);
+    }
+
+}
+
+
