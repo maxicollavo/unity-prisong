9422e49f8ffa4bd5ff120dc3d1463bc2ab684c1b
bauutyyy <bautyakerman@gmail.com>
2023-08-06 00:11:55 -0300

    gg

---
+++ Prisong/Assets/Powervelocida.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powervelocida : MonoBehaviour
+{
+
+    public bool destruirConCursor;
+    public bool destruirAutomatico;
+    public 
+    
+    
+    
+    
+    
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
