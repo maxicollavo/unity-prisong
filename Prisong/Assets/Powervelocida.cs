8354e7c3681ff0a8200d6814aba60288dc7a53e8
bauutyyy <bautyakerman@gmail.com>
2023-08-06 21:22:58 -0300

    GG

--- Prisong/Assets/Powervelocida.cs
+++ Prisong/Assets/Powervelocida.cs
@@ -1,29 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class Powervelocida : MonoBehaviour
 {
 
     public bool destruirConCursor;
     public bool destruirAutomatico;
-    public 
-    
-    
-    
-    
-    
-    
-    
-    
+    public PlayerInputManager playerInputManager;
+
+    public int aumentavelocidad; 
+
     void Start()
     {
-        
+        playerInputManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInputManager>();
+
+
     }
 
     
     void Update()
     {
         
+    
+    
+    
+    
+    
     }
+
+    public void Efecto()
+    {
+        PlayerInputManager.spe += 80;
+
+
+
+
+    }
+
+
+
+
 }
